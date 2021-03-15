72d80604a05728d888da1a88139b590681260193
bukunmialuko <oluwabukunmi.aluko@gmail.com>
2021-03-15 13:38:38 +0100

    Minor edits

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductsListComponent } from './products-list/products-list.component';
+import { ProductRowComponent } from './product-row/product-row.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsListComponent,
+    ProductRowComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/product-row/product-row.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'product-row',
+  templateUrl: './product-row.component.html',
+  styleUrls: ['./product-row.component.css']
+})
+export class ProductRowComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
